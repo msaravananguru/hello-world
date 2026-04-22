6fb60d99f94f7c75a12b49510bbd8460260bc005
msaravananguru <saravanan24121998@gmail.com>
2026-04-22 06:35:28 +0530

    Updated HelloWorld print message

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,9 @@
 package randomTask;
 
 public class HelloWorld {
+	String Hello() {
+		return "Hello";
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Big Dream or Plan...");
