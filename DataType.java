37c4bb4c71e5011b6ef5de753e95c0e7a24e46c2
msaravananguru <saravanan24121998@gmail.com>
2026-04-22 06:22:49 +0530

    Initial commit - Java files

---
+++ DataType.java
@@ -0,0 +1,18 @@
+package randomTask;
+
+public class DataType {
+	
+	DataType(){
+		int a=110;
+		Integer b=110;
+		System.out.println(a);
+		System.out.println(b);
+		Integer count = null;
+//		int x = count;
+		
+	}
+
+	public static void main(String[] args) {
+		DataType dt= new DataType();
+	}
+}
---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+package randomTask;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Big Dream or Plan...");
+	}
+}
